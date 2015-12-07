47f9970d7401794440216303f9c59077ff84b714
Ana Luiza Cunha <mcunha.ana@gmail.com>
2015-12-07 16:48:54 -0500

    Add Events and Categories models

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,7 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+  
+  def events
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root                 'static_pages#home'
   get     'about'   => 'static_pages#about'
   get     'help'    => 'static_pages#help'
+  
+  get 'events/index'
+  get 'events/show'
 end
