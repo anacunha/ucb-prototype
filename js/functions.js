782c3824379924bc83838961d7ccf15d151291c8
stefanolafs <fani5000@gmail.com>
2015-12-06 23:38:48 -0500

    Bootstrap meta tag added + more stuff
    The edit entry toolbar is more structured now.

--- js/functions.js
+++ js/functions.js
@@ -1,30 +1,5 @@
 
 $( window ).load(function() {
-	$(".diaryEvent img").each(function(){
-		var imgHeight = $(this).height();
-		$(this).parent().height(imgHeight);
-	});
-});
-
-$( window ).load(function() {
-
-	$(".diaryEvent .more").click(function(){
-		if($(this).html().indexOf("less") >= 0){
-			var newHeight = $(this).parent().find("img").height();
-			var textElem = $(this).parent().find(".text");
-			textElem.height(newHeight);
-			textElem.css("fontSize", "2.5em");
-			$(this).html("... more");
-			$(this).css("fontSize", "2.5em");
-		} else {
-			$(this).parent().find(".text").height('auto');
-			var textElem = $(this).parent().find(".text");
-			textElem.height(newHeight);
-			textElem.css("fontSize", "1em");
-			$(this).html("... less");
-			$(this).css("fontSize", "1em");
-		}
-	});
 	
 	$(".cameraButton").click(function () {
 		$(".photoMenu").toggle();
