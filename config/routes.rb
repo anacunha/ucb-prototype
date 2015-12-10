7a82e1ac59f92e31a74c87cdd368990235afce33
Ana Luiza Cunha <mcunha.ana@gmail.com>
2015-12-10 12:29:33 -0500

    User login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  
   root                 'static_pages#home'
   get     'about'   => 'static_pages#about'
   get     'help'    => 'static_pages#help'
+  get     'signup'  => 'users#new'
+  get     'login'   => 'sessions#new'
+  post    'login'   => 'sessions#create'
+  delete  'logout'  => 'sessions#destroy'
+  resources :users
   
   get 'events/index'
   get 'events/show'
-  
-  get 'signup'  => 'users#new'
-  resources :users
 end
