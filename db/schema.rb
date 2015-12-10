57e6d4a20c733a1469ce3075a900a40b7c3cefd0
Ana Luiza Cunha <mcunha.ana@gmail.com>
2015-12-10 12:51:26 -0500

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210145639) do
+ActiveRecord::Schema.define(version: 20151210172943) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20151210145639) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
