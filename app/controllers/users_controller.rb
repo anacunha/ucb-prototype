580130a62dd04bad937c0d2b61927a879e4d77b0
Ana Luiza Cunha <mcunha.ana@gmail.com>
2015-12-10 11:56:30 -0500

    Finish user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to UCB!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :partner_organization, :gender, :neighborhood, :workplace, :phone, :address, :city, :state, :zip_code)
+    end
+end
