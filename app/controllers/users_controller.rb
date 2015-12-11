69d37ecd27c7e9fa52116d5cd5c8fb13a4cd48c8
Ana Luiza Cunha <mcunha.ana@gmail.com>
2015-12-11 00:52:29 -0500

    User Profile editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:personal, :contact, :update]
+  before_action :correct_user,   only: [:personal, :contact, :update]
 
   def show
     @user = User.find(params[:id])
@@ -6,6 +8,16 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+  
+  def personal
+    @user = User.find(params[:id])
+    session[:return_to] = 'personal'
+  end
+  
+  def contact
+    @user = User.find(params[:id])
+    session[:return_to] = 'contact'
   end
   
   def create
@@ -19,9 +31,35 @@ class UsersController < ApplicationController
     end
   end
   
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render session.delete(:return_to)
+    end
+  end
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :partner_organization, :gender, :neighborhood, :workplace, :phone, :address, :city, :state, :zip_code)
+    end
+  
+    # Before filters
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+
+    # Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   
+  # Static Pages
   root                 'static_pages#home'
   get     'about'   => 'static_pages#about'
   get     'help'    => 'static_pages#help'
-  get     'signup'  => 'users#new'
+  
+  # Sessions
   get     'login'   => 'sessions#new'
   post    'login'   => 'sessions#create'
   delete  'logout'  => 'sessions#destroy'
+  
+  # Users
   resources :users
+  get 'signup'              => 'users#new'
+  get 'users/:id/personal'  => 'users#personal'
+  get 'users/:id/contact'   => 'users#contact'
   
   get 'events/index'
   get 'events/show'
