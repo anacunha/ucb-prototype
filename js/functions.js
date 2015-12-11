796a4bba590e86b6ec9fc839d3d851ea2f0335d5
stefanolafs <fani5000@gmail.com>
2015-12-11 02:01:38 -0500

    A few things
    Location added, map, reply button and styling, I'm Going button color
    thing.

--- js/functions.js
+++ js/functions.js
@@ -47,8 +47,35 @@ $( window ).load(function() {
 			});
 		}
 	});
+	
+
 });
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+
+$(document).ready(function(){
+	$(".reply").click(function(){
+		$(this).parents(".post").find(".replyPanel").slideDown(500);
+	});
+	
+	$("#going-button").click(function(){
+		if(!$(this).isBgColor("#5CC0DE")){
+			$(this).css("background-color", "#5CC0DE");
+		} else {
+			$(this).css("background-color", "#DDDDDC");
+		}
+	});
+});
+
+(function($) {
+    $.fn.isBgColor = function(color) {
+        var thisBgColor = this.eq(0).css('backgroundColor');
+        var computedColor = $('<div/>').css({ 
+            backgroundColor: color
+        }).css('backgroundColor');
+        return thisBgColor === computedColor;
+    }
+})(jQuery);
