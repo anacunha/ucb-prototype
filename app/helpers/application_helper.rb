6d2048edd9f947d00c2093afb86b4fa518b17598
Ana Luiza Cunha <mcunha.ana@gmail.com>
2015-12-12 18:42:38 -0500

    Does not display profile picture if user doesn't have one

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
       base_title + " | " + page_title
     end
   end
+  
+  # Returns true if an asset exists
+  def asset_exists?(path)
+    begin
+      pathname = Rails.application.assets.find_asset(path).nil?
+      return !pathname
+    rescue Sprockets::FileNotFound
+      return false
+    end
+  end
 end
