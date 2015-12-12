b080821ea02bacd1e94c9bc23cc2283d9f97d920
Ana Luiza Cunha <mcunha.ana@gmail.com>
2015-12-11 22:31:06 -0500

    Saving Diary Entries (finally)

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,15 +1,45 @@
 class DiariesController < ApplicationController
+  
   def index
     @diaries = Diary.where('user_id == ?', current_user.id)
     @events = current_user.events
   end
   
+  def new
+    @diary = Diary.new
+    @event_id = params[:event]
+  end
+  
   def create
+    @diary = Diary.new(diary_params)
+    if @diary.save
+      flash[:success] = "Diary Entry created"
+      redirect_to diaries_url
+    else
+      flash.now[:danger] = "Coudln't save Diary Entry at this time"
+      render 'new'
+    end
   end
   
   def edit
+    @diary = Diary.find(params[:id])
   end
+  
+  def update
+    @diary = Diary.find(params[:id])
+    
+    if @diary.update_attributes(diary_params)
+      flash[:success] = "Diary Entry updated"
+      redirect_to diaries_url
+    else
+      flash.now[:danger] = "Coudln't update Diary Entry at this time"
+      render 'update'
+    end
+  end
+  
+  private
 
-  def show
-  end
+    def diary_params
+      params.require(:diary).permit(:user_id, :event_id, :entry)
+    end
 end
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,8 @@
 class Diary < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
+  
+  validates :user_id,   presence: true
+  validates :event_id,  presence: true
+  validates :entry,     presence: true
 end
